b6b7205885867f79240a7491d903db0caaea3ef4
Jonas Huber <jonas.huber@rl-institut.de>
2024-04-08 13:30:19 +0200

    remove whitespace

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ createdb oedb
 
 psql -U postgres -d oedb -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 psql -U postgres -d oedb -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
-psql -U postgres -d oedb -c " CREATE EXTENSION IF NOT EXISTS hstore;"
+psql -U postgres -d oedb -c "CREATE EXTENSION IF NOT EXISTS hstore;"
 psql -U postgres -d oedb -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
 
 psql -U postgres -d oep_django -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 psql -U postgres -d oep_django -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
-psql -U postgres -d oep_django -c " CREATE EXTENSION IF NOT EXISTS hstore;"
+psql -U postgres -d oep_django -c "CREATE EXTENSION IF NOT EXISTS hstore;"
 psql -U postgres -d oep_django -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
